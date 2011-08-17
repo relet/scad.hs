d915881979693ff6fe99f6278a1c59913b902fd5
Thomas Hirsch <fwd.github.20.ravn@neverbox.com>
2011-08-17 15:43:49 +0200

    useful test, classification in/out seems to fail under some circumstances

--- test.hs
+++ test.hs
@@ -1,10 +1,9 @@
 import Scad
 import Csg hiding (Polyhedron)
 
-main = print $ render $ c /+ c2
+main = print $ render $ (c/>(0.5,0.5,0)) /- c2
 
 c  = Polyhedron Csg.cube
 c2 = Polyhedron $ Csg.poly pts cube_tris
-     where pts = map (\pt -> [pt!!0 + sin(pi/8) - 0.5, pt!!1 + cos(pi/8) - 0.5, pt!!2]) cube_pts
-
-
+     where pts = map (\p -> [(0.5::Double) + 0.5 * (sin $ angle p + pi/4), 0.5 + 0.5 * (cos $ angle p + pi/4), p!!2]) cube_pts
+           angle p = atan2 (p!!1-0.5) (p!!0-0.5)
